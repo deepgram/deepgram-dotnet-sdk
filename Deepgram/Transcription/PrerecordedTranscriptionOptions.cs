3fa765c0ba6ec4756d52bd719c98afd19320ed9f
Brian Barrow <briancbarrow@gmail.com>
2023-04-10 13:24:43 -0600

    updated sentiment types

--- Deepgram/Transcription/PrerecordedTranscriptionOptions.cs
+++ Deepgram/Transcription/PrerecordedTranscriptionOptions.cs
@@ -228,6 +228,12 @@ namespace Deepgram.Transcription
         /// Indicates whether Deepgram will identify sentiment in the transcript.
         /// </summary>
         [JsonProperty("analyze_sentiment")]
+        public Nullable<bool> AnalyzeSentiment { get; set; } = null;
+
+        /// <summary>
+        /// Indicates whether Deepgram will identify sentiment in the audio.
+        /// </summary>
+        [JsonProperty("sentiment")]
         public Nullable<bool> Sentiment { get; set; } = null;
 
         /// <summary>
