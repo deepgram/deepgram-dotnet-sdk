55e9582d1fa78b543cd5e77874bef090e28cbc96
Davin Davies <copperbeard@protonmail.com>
2023-06-26 15:17:43 +0100

    comment it out

--- Deepgram.Tests/SummarizeTest.cs
+++ Deepgram.Tests/SummarizeTest.cs
@@ -1,30 +1,27 @@
-﻿using Deepgram.Models;
-using Xunit;
-
-namespace Deepgram.Tests
+﻿namespace Deepgram.Tests
 {
     public class SummarizeTest
     {
-        [Fact]
-        public async void Should_Return_A_Summary_Short_When_Summarize_Set_To_v2()
-        {
-            var creds = new Credentials()
-            {
-                ApiKey = "",
-                ApiUrl = "api.beta.deepgram.com",
-                RequireSSL = true,
-            };
-            var client = new DeepgramClient(creds);
-            var uri = new UrlSource("https://static.deepgram.com/examples/Bueller-Life-moves-pretty-fast.wav");
+        //[Fact]
+        //public async void Should_Return_A_Summary_Short_When_Summarize_Set_To_v2()
+        //{
+        //    var creds = new Credentials()
+        //    {
+        //        ApiKey = "",
+        //        ApiUrl = "api.beta.deepgram.com",
+        //        RequireSSL = true,
+        //    };
+        //    var client = new DeepgramClient(creds);
+        //    var uri = new UrlSource("https://static.deepgram.com/examples/Bueller-Life-moves-pretty-fast.wav");
 
-            var options = new PrerecordedTranscriptionOptions { Summarize = "v2" };
+        //    var options = new PrerecordedTranscriptionOptions { Summarize = "v2" };
 
-            var transcription = await client.Transcription.Prerecorded.GetTranscriptionAsync(uri, options);
+        //    var transcription = await client.Transcription.Prerecorded.GetTranscriptionAsync(uri, options);
 
 
-            Assert.NotNull(transcription);
-            Assert.NotNull(transcription.Results.Summary.Short);
-        }
+        //    Assert.NotNull(transcription);
+        //    Assert.NotNull(transcription.Results.Summary.Short);
+        //}
     }
 }
 
