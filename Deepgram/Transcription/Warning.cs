264349fa8b20ad520eedf3b7565da12d35403100
Sandra Rodgers <sandra.rodgers@deepgram.com>
2023-07-19 14:17:20 -0500

    formatting

--- Deepgram/Transcription/Warning.cs
+++ Deepgram/Transcription/Warning.cs
@@ -4,28 +4,28 @@ using Newtonsoft.Json.Converters;
 
 namespace Deepgram.Transcription
 {
-    public class Warning
-    {
-        /// <summary>
-        /// Parameter sent in the request that resulted in the warning
-        /// </summary>
-        [JsonProperty("parameter")]
-        public string Parameter { get; set; }
+  public class Warning
+  {
+      /// <summary>
+      /// Parameter sent in the request that resulted in the warning
+      /// </summary>
+      [JsonProperty("parameter")]
+      public string Parameter { get; set; }
 
-        /// <summary>
-        /// The type of warning
-        /// </summary>
-        [JsonProperty("type")]
-        [JsonConverter(typeof(StringEnumConverter))]
-        public WarningType Type { get; set; }
+      /// <summary>
+      /// The type of warning
+      /// </summary>
+      [JsonProperty("type")]
+      [JsonConverter(typeof(StringEnumConverter))]
+      public WarningType Type { get; set; }
 
-        /// <summary>
-        /// The warning message
-        /// </summary>
-        [JsonProperty("message")]
-        public string Message { get; set; }
+      /// <summary>
+      /// The warning message
+      /// </summary>
+      [JsonProperty("message")]
+      public string Message { get; set; }
 
-    }
+  }
 }
 
 
--- Deepgram/Transcription/WarningType.cs
+++ Deepgram/Transcription/WarningType.cs
@@ -6,18 +6,18 @@ namespace Deepgram.Transcription
     public enum WarningType
 
     {
-    [EnumMember(Value = "unsupported_language")]
-    UnsupportedLanguage,
-    
-    [EnumMember(Value = "unsupported_model")]
-    UnsupportedModel,
-    
-    [EnumMember(Value = "unsupported_encoding")]
-    UnsupportedEncoding,
-    
-    [EnumMember(Value = "deprecated")]
-    Deprecated
-}
+        [EnumMember(Value = "unsupported_language")]
+        UnsupportedLanguage,
+        
+        [EnumMember(Value = "unsupported_model")]
+        UnsupportedModel,
+        
+        [EnumMember(Value = "unsupported_encoding")]
+        UnsupportedEncoding,
+        
+        [EnumMember(Value = "deprecated")]
+        Deprecated
+    }
 }
 
 
