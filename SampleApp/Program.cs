cd223dfe54fa6bfc5775738b63e98d9701802e5f
Sandra Rodgers <sandra.rodgers@deepgram.com>
2023-07-19 13:57:54 -0500

    adds warning object

--- SampleApp/Program.cs
+++ SampleApp/Program.cs
@@ -5,30 +5,20 @@ namespace SampleApp
 {
     class Program
     {
-        const string API_KEY = "YOUR_DEEPGRAM_API_KEY";
+        const string API_KEY = "24c13ac94b54bd2345dc5d5a93488de63d7d6531";
 
         static async Task Main(string[] args)
-        {   
-            var credentials = new Credentials(API_KEY);
-            DeepgramClient deepgram = new DeepgramClient(credentials);
-
-            // UNCOMMENT IF USING LOCAL FILE:
-            // using (FileStream fs = File.OpenRead("preamble.wav"))
-            {
-                var response = await deepgram.Transcription.Prerecorded.GetTranscriptionAsync(
-                    // UNCOMMENT IF USING LOCAL FILE:
-                    // new Deepgram.Transcription.StreamSource(
-                    //     fs,
-                    //     "audio/wav"),
-                    new Deepgram.Transcription.UrlSource("https://static.deepgram.com/examples/Bueller-Life-moves-pretty-fast.wav"),
+        {
+            DeepgramClient deepgram = new DeepgramClient(new Credentials(API_KEY));
+            var response = await deepgram.Transcription.Prerecorded.GetTranscriptionAsync(
+                    new UrlSource("https://www.happyhourspanish.com/wp-content/uploads/podcasts/HHS_Podcast_Soccer_Eurocup.mp3"),
                     new PrerecordedTranscriptionOptions()
                     {
-                        Punctuate = true,
-                        Utterances = true,
+                        Summarize = "v2",
+                        DetectLanguage = true
                     });
 
-                Console.WriteLine(JsonConvert.SerializeObject(response));
-            }
+            Console.WriteLine(JsonConvert.SerializeObject(response));
         }
     }
 }
