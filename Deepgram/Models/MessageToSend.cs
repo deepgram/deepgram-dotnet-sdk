a1cf136b6ed2d33745a5d25d5c9f6ad7134a045c
Jason Maldonis <jjmaldonis@gmail.com>
2023-09-08 13:43:29 -0500

    fix: bugfix to send KeepAlive messages as text rather than binary

--- Deepgram/Models/MessageToSend.cs
+++ Deepgram/Models/MessageToSend.cs
@@ -1,14 +1,18 @@
 ﻿using System;
+using System.Net.WebSockets;
 
 namespace Deepgram.Models
 {
     internal readonly struct MessageToSend
     {
-        public MessageToSend(byte[] message)
+        public MessageToSend(byte[] message, WebSocketMessageType type)
         {
             Message = new ArraySegment<byte>(message);
+            MessageType = type;
         }
 
-        public ArraySegment<byte> Message { get; }
+        public readonly ArraySegment<byte> Message { get; }
+
+        public readonly WebSocketMessageType MessageType { get; }
     }
 }
