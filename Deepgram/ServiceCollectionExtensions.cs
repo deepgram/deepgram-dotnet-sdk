f01ea20f813ec7102a637be6a9fdbbf8bb54e218
Thindal <thindaltv@gmail.com>
2023-12-06 21:22:48 +0100

    Registering clients.

--- Deepgram/ServiceCollectionExtensions.cs
+++ Deepgram/ServiceCollectionExtensions.cs
@@ -4,6 +4,12 @@ public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddDeepgram(this IServiceCollection services)
     {
+        services.AddTransient<LiveClient>();
+        services.AddTransient<PrerecordedClient>();
+        services.AddTransient<LiveClient>();
+
+        services.AddTransient<ManageClient>();
+
         services.AddHttpClient(Constants.HTTPCLIENT_NAME)
             .AddTransientHttpErrorPolicy(policyBuilder =>
             policyBuilder.WaitAndRetryAsync(Backoff.DecorrelatedJitterBackoffV2(TimeSpan.FromSeconds(1), 5)));
