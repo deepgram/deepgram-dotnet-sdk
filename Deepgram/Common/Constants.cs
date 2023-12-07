b47b5182c863d910efa08371db2b64286ed554bf
Thindal <thindaltv@gmail.com>
2023-12-07 20:40:44 +0100

    Removed references to api key.

--- Deepgram/Common/Constants.cs
+++ Deepgram/Common/Constants.cs
@@ -5,7 +5,6 @@
 
         public const string DEFAULT_URI = "api.deepgram.com";
         public const string API_VERSION = "v1";
-        public const string APIKEY_ENVIRONMENT_NAME = "DEEPGRAM_API_KEY";
         public const string HTTPCLIENT_NAME = "DEEPGRAM_HTTP_CLIENT";
         public const string WSSCLIENT_NAME = "DEEPGRAM_WSS_CLIENT";
         public const string DEFAULT_CONTENT_TYPE = "application/json";
