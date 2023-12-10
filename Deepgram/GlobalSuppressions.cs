5f7d55d5a63e973d47f77e08ec2229910358f730
Davin Davies <copperbeard@protonmail.com>
2023-12-10 20:39:20 +0000

    clean up

--- Deepgram/GlobalSuppressions.cs
+++ Deepgram/GlobalSuppressions.cs
@@ -5,15 +5,4 @@
 
 using System.Diagnostics.CodeAnalysis;
 
-
-/* Unmerged change from project 'Deepgram (net8.0)'
-Added:
-[assembly: SuppressMessage("Performance", "SYSLIB1045:Convert to 'GeneratedRegexAttribute'.", Justification = "not used as not compatible with earlier frameworks that are supported by SDK", Scope = "member", Target = "~M:Deepgram.Extensions.HttpClientExtensions.SetBaseUrl(System.Net.Http.HttpClient,Deepgram.Models.DeepgramClientOptions)")]
-[assembly: SuppressMessage("Performance", "SYSLIB1045:Convert to 'GeneratedRegexAttribute'.", Justification = "not used as not compatible with earlier frameworks that are supported by SDK", Scope = "member", Target = "~M:Deepgram.LiveClient.GetBaseUrl(Deepgram.Models.DeepgramClientOptions)~System.String")]
-[assembly: SuppressMessage("Performance", "SYSLIB1045:Convert to 'GeneratedRegexAttribute'.", Justification = "not used as not compatible with earlier frameworks that are supported by SDK", Scope = "member", Target = "~M:Deepgram.Utilities.UserAgentUtil.GetInfo~System.String")]
-*/
-[assembly: SuppressMessage("GeneratedRegex", "SYSLIB1045:Convert to 'GeneratedRegexAttribute'.", Justification = "not used as not compatible with earlier frameworks that are supported by SDK", Scope = "member", Target = "~M:Deepgram.Extensions.HttpClientExtensions.SetBaseUrl(System.Net.Http.HttpClient,Deepgram.Models.DeepgramClientOptions)")]
-[assembly: SuppressMessage("GeneratedRegex", "SYSLIB1045:Convert to 'GeneratedRegexAttribute'.", Justification = "not used as not compatible with earlier frameworks that are supported by SDK", Scope = "member", Target = "~M:Deepgram.LiveClient.GetBaseUrl(Deepgram.Models.DeepgramClientOptions)~System.String")]
-[assembly: SuppressMessage("GeneratedRegex", "SYSLIB1045:Convert to 'GeneratedRegexAttribute'.", Justification = "not used as not compatible with earlier frameworks that are supported by SDK", Scope = "member", Target = "~M:Deepgram.Utilities.UserAgentUtil.GetInfo~System.String")]
-[assembly: SuppressMessage("Style", "IDE0290:Use primary constructor", Justification = "Not suitable for the type", Scope = "member", Target = "~M:Deepgram.DeepgramEventArgs.ConnectionErrorEventArgs.#ctor(System.Exception)")]
-[assembly: SuppressMessage("Style", "IDE0057:Use range operator", Justification = "<Pending>", Scope = "member", Target = "~M:Deepgram.Records.PreRecorded.SyncPrerecordedResponse.SecondsToTimestamp(System.Decimal)~System.String")]
+[assembly: SuppressMessage("Style", "IDE0290:Use primary constructor", Justification = "<Pending>", Scope = "member", Target = "~M:Deepgram.DeepgramEventArgs.ConnectionErrorEventArgs.#ctor(System.Exception)")]
