12fdcdd481ecd15ccaab5ed4765d77f053a7f141
dvonthenen <david.vonthenen@gmail.com>
2024-02-04 11:32:23 -0800

    Audit of Auth/OnPrem Model Classes

--- Deepgram/Models/OnPrem/v1/DistributionCredentials.cs
+++ Deepgram/Models/OnPrem/v1/DistributionCredentials.cs
@@ -2,18 +2,33 @@
 
 public record DistributionCredentials
 {
+    /// <summary>
+    /// TODO
+    /// </summary>
     [JsonPropertyName("distribution_credentials_id")]
     public string? DistributionCredentialsId { get; set; }
 
+    /// <summary>
+    /// TODO
+    /// </summary>
     [JsonPropertyName("provider")]
     public string? Provider { get; set; }
 
+    /// <summary>
+    /// TODO
+    /// </summary>
     [JsonPropertyName("comment")]
     public string? Comment { get; set; }
 
+    /// <summary>
+    /// TODO
+    /// </summary>
     [JsonPropertyName("scopes")]
-    public IReadOnlyList<string> Scopes { get; set; }
+    public IReadOnlyList<string>? Scopes { get; set; }
 
+    /// <summary>
+    /// TODO
+    /// </summary>
     [JsonPropertyName("created")]
     public string? Created { get; set; }
 }
