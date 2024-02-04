71588bc63657c77660342c4c00b9de161edc47bc
dvonthenen <david.vonthenen@gmail.com>
2024-02-04 14:43:04 -0800

    Temporary Fix for v3 Timeouts

--- Deepgram.Tests/UtilitiesTests/HttpClientUtilTests.cs
+++ Deepgram.Tests/UtilitiesTests/HttpClientUtilTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Net.Http;
 using Deepgram.Utilities;
 using Xunit;
 
@@ -21,7 +22,62 @@ namespace Deepgram.Tests.UtilitiesTests
             Assert.IsAssignableFrom<HttpClient>(result);
             Assert.Equal("application/json", result.DefaultRequestHeaders.Accept.ToString());
             Assert.Equal(agent, result.DefaultRequestHeaders.UserAgent.ToString());
+        }
 
+        [Fact]
+        public async Task ExecuteAsync_NotExecutedWithinTimeout_ThrowsExceptionAsync()
+        {
+            // Arrange
+            var endpoint = "https://google.com/";
+            var timeoutShouldFail = TimeSpan.FromMilliseconds(1);
+
+            var httpClientUtil = new HttpClientUtil();
+            httpClientUtil.SetTimeOut(timeoutShouldFail);
+
+            var client = httpClientUtil.HttpClient;
+            bool resultShouldFail = false;
+
+            // Act
+            try
+            {
+                await client.GetAsync(endpoint);
+            }
+            catch (TaskCanceledException)
+            {
+                // we are expecting this to timeout
+                resultShouldFail = true;
+            }
+
+            // Assert
+            Assert.True(resultShouldFail);
+        }
+
+        [Fact]
+        public async Task ExecuteAsync_ExecutedWithinTimeout_ThrowsExceptionAsync()
+        {
+            // Arrange
+            var endpoint = "https://google.com/";
+            var timeoutShouldSucceed = TimeSpan.FromMilliseconds(2000);
+
+            var httpClientUtil = new HttpClientUtil();
+            httpClientUtil.SetTimeOut(timeoutShouldSucceed);
+
+            var client = httpClientUtil.HttpClient;
+            bool resultShouldSucceed = false;
+
+            // Act
+            try
+            {
+                await client.GetAsync(endpoint);
+            }
+            catch (TaskCanceledException)
+            {
+                // we should not see the exception thrown
+                resultShouldSucceed = true;
+            }
+
+            // Assert
+            Assert.False(resultShouldSucceed);
         }
     }
 }
