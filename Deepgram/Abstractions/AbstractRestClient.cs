8761ba5490cf0f962d4385f479f4063c90192ac3
dvonthenen <david.vonthenen@gmail.com>
2024-02-07 11:13:41 -0800

    Update Copyright 2024, Not 2023 (Sigh)

--- Deepgram/Abstractions/AbstractRestClient.cs
+++ Deepgram/Abstractions/AbstractRestClient.cs
@@ -1,4 +1,4 @@
-﻿// Copyright 2021-2023 Deepgram .NET SDK contributors. All Rights Reserved.
+﻿// Copyright 2021-2024 Deepgram .NET SDK contributors. All Rights Reserved.
 // Use of this source code is governed by a MIT license that can be found in the LICENSE file.
 // SPDX-License-Identifier: MIT
 
