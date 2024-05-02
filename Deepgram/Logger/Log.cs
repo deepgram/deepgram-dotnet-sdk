67d05a7c9efb5198b3e8e35280fb35e214e23bc9
Nick Ganju <nick.ganju@gmail.com>
2024-05-02 17:34:59 -0400

    update with PR feedback to Logger.Initialize

--- Deepgram/Logger/Log.cs
+++ Deepgram/Logger/Log.cs
@@ -18,7 +18,15 @@ public sealed class Log
     }
     public static void Initialize(ILogger logger)
     {
+        if (logger == null) throw new ArgumentNullException(nameof(logger));
+        if (instance != null)
+        {
+            Log.Warning("Logger", "Attempt to re-initialize logger ignored.");
+            return;
+        }
+        
         instance = logger;
+        Log.Information("Logger", "Logger initialized.");
     }
     /// <summary>
     /// Initializes the logger with the specified log level and filename.
