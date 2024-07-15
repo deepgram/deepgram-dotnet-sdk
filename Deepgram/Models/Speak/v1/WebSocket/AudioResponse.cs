a4f57e4592cfa13ae57b61b89b17e61e0a0c2b4b
dvonthenen <david.vonthenen@gmail.com>
2024-07-15 11:01:27 -0700

    [HOLD] Reintroduce TTS WS

---
+++ Deepgram/Models/Speak/v1/WebSocket/AudioResponse.cs
@@ -0,0 +1,29 @@
+﻿// Copyright 2024 Deepgram .NET SDK contributors. All Rights Reserved.
+// Use of this source code is governed by a MIT license that can be found in the LICENSE file.
+// SPDX-License-Identifier: MIT
+
+namespace Deepgram.Models.Speak.v1.WebSocket;
+
+public record AudioResponse : IDisposable
+{
+    /// <summary>
+    /// Open event type.
+    /// </summary>
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+	[JsonPropertyName("type")]
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public SpeakType? Type { get; set; } = SpeakType.Audio;
+
+    /// <summary>
+    /// A stream of the audio file
+    /// </summary>
+    public MemoryStream? Stream { get; set; }
+
+    // NOTE: There isn't a ToString() function because this will cause an odd Exception to be thrown:
+    // InvalidOperationException: "Timeouts are not supported on this stream."
+
+    public void Dispose()
+    {
+        Stream?.Dispose();
+    }
+}
---
+++ Deepgram/Models/Speak/v1/WebSocket/ClearedResponse.cs
@@ -0,0 +1,31 @@
+﻿// Copyright 2024 Deepgram .NET SDK contributors. All Rights Reserved.
+// Use of this source code is governed by a MIT license that can be found in the LICENSE file.
+// SPDX-License-Identifier: MIT
+
+namespace Deepgram.Models.Speak.v1.WebSocket;
+
+public record ClearedResponse
+{
+    /// <summary>
+    /// Clear event type.
+    /// </summary>
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+	[JsonPropertyName("type")]
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public SpeakType? Type { get; set; } = SpeakType.Cleared;
+
+    /// <summary>
+    /// Sequence ID
+    /// </summary>
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    [JsonPropertyName("sequence_id")]
+    public int? SequenceId { get; set; }
+
+    /// <summary>
+    /// Override ToString method to serialize the object
+    /// </summary>
+    public override string ToString()
+    {
+        return Regex.Unescape(JsonSerializer.Serialize(this, JsonSerializeOptions.DefaultOptions));
+    }
+}
