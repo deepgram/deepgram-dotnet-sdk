d2af56a1a06c56b07ba454d8be0594572596066f
dvonthenen <david.vonthenen@gmail.com>
2024-07-31 09:01:36 -0700

    Implement Models API

---
+++ Deepgram/Models/Manage/v1/ModelsResponse.cs
@@ -0,0 +1,22 @@
+﻿// Copyright 2024 Deepgram .NET SDK contributors. All Rights Reserved.
+// Use of this source code is governed by a MIT license that can be found in the LICENSE file.
+// SPDX-License-Identifier: MIT
+
+namespace Deepgram.Models.Manage.v1;
+
+public record ModelsResponse
+{
+    /// <summary>
+    /// Contains the Speech-to-Text models
+    /// </summary>
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    [JsonPropertyName("stt")]
+    public List<Stt>? Stt { get; set; }
+
+    /// <summary>
+    /// Contains the Text-to-Speech models
+    /// </summary>
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    [JsonPropertyName("tts")]
+    public List<Tts>? Tts { get; set; }
+}
